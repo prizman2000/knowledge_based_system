42753574bbadd3827e0f7534adf8d195f34a5c98
neu_net <przman2000@mail.ru>
2022-03-14 15:28:53 +1000

    completed Class block

--- client/src/components/Class/Class.js
+++ client/src/components/Class/Class.js
@@ -1,11 +1,38 @@
 import React from "react";
 import s from './Class.module.scss';
+import {List, Button, Input} from 'antd';
 
 export default function Class() {
 
+    const data = [
+        'Классический',
+        'Спортивный',
+        'Кэжуал',
+        'Минимализм',
+        'Гламур',
+    ];
+
     return (
         <>
-            Class
+            <Input.Group className={s.add_field} compact>
+                <Input className={s.input} placeholder={'Введите название класса'} />
+                <Button className={s.button} type="primary">Добавить</Button>
+            </Input.Group>
+            <List
+                className={s.list}
+                size={'large'}
+                borded
+                dataSource={data}
+                renderItem={item =>
+                    <List.Item className={s.list_item}>
+                        <List.Item.Meta
+                            title={item}/>
+                        <Button danger>Удалить</Button>
+                    </List.Item>
+                }
+            >
+
+            </List>
         </>
     );
 }
